e76ed2d211d486741b1083123171e28bbdcc9dae
Eden Marco <emarco177@gmail.com>
2023-04-28 08:32:33 +0300

    implemented memory

--- backend/core.py
+++ backend/core.py
@@ -1,9 +1,9 @@
 import os
-from typing import Any
+from typing import Any, List, Dict
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
 from langchain.vectorstores import Pinecone
 import pinecone
 
@@ -15,19 +15,18 @@ pinecone.init(
 )
 
 
-def run_llm(query: str) -> Any:
+def run_llm(query: str, chat_history: List[Dict[str, Any]] = []) -> Any:
     embeddings = OpenAIEmbeddings()
     docsearch = Pinecone.from_existing_index(
         index_name=INDEX_NAME, embedding=embeddings
     )
     chat = ChatOpenAI(verbose=True, temperature=0)
-    qa = RetrievalQA.from_chain_type(
-        llm=chat,
-        chain_type="stuff",
-        retriever=docsearch.as_retriever(),
-        return_source_documents=True,
+
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
     )
-    return qa({"query": query})
+
+    return qa({"question": query, "chat_history": chat_history})
 
 
 if __name__ == "__main__":
